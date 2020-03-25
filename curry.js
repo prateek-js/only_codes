e5a61b8caa09b4142cf2f480716043a373f7de78
Prateek Desai <prateekdesai1990@gmail.com>
2020-03-25 22:46:44 +0530

    Update curry.js

--- curry.js
+++ curry.js
@@ -18,3 +18,24 @@ function sum(a, b, c) {
 let curriedSum = curry(sum);
 
 console.log(curriedSum(1, 2, 3));
+
+
+var infiniteCurry = (fn, seed) => {
+  var reduceValue = (args, seedValue) =>
+    args.reduce((acc, a) => {
+      return fn.call(fn, acc, a);
+    }, seedValue);
+  var next = (...args) => {
+    return (...x) => { console.log(x[0])
+      if (!x.length) {
+        return reduceValue(args, seed);
+      }
+      return next(...args, reduceValue(x, seed));
+    };
+  };
+  return next();
+};
+
+var iSum = infiniteCurry((x, y) => x + y, 0);
+
+console.log(iSum(1)(3, 4)(5, 6)(7, 8, 9)());
